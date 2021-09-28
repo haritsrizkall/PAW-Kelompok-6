ceae1fcccb7f8aa97acaba2cb17c22e56e472b55
haningjihan <haningjihan00@mail.ugm.ac.id>
2021-09-28 16:15:16 +0700

    activity controller & router

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const PORT = 3000;
 const morgan = require('morgan');
 const config = require('./config');
 const database = require('./database/client');
+const Activity = require('./database/model/Activity');
 const User = require('./router/Users');
 database.connect();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(morgan('combined'));
 
 app.use('/users', User);
+app.use('/activities', Activity);
 app.get('/', (req, res) => {
     res.send(`Please ${config.database.uri}`);
 });
