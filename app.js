42d5ee94d0f0f3b62876bb4b485bc01a5c89e819
haritsrizkalll <rizkal@moneyhub.id>
2021-11-23 22:38:18 +0700

    cors

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
 const morgan = require('morgan');
 const config = require('./config');
 const database = require('./database/client');
 const Activity = require('./router/Activity');
 const User = require('./router/Users');
+const cors = require('cors');
 
 database.connect();
 
@@ -14,6 +14,7 @@ app.use(morgan('combined'));
 
 app.use('/users', User);
 app.use('/activities', Activity);
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send(`Please ${config.database.uri}`);
